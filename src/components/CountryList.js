c22aac964474b9ba09d078c67125b20c6a9a3dd3
Haider <malikhaiderkhan81@gmail.com>
2023-08-24 02:03:19 +0500

    add search bar and filtering the country by searching

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCountryData } from '../redux/countries/countriesSlice';
@@ -9,16 +9,33 @@ function CountryList() {
   const countries = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.loading);
 
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
   useEffect(() => {
     dispatch(fetchCountryData());
   }, [dispatch]);
 
+  useEffect(() => {
+    const filtered = countries.filter((country) => country.name.common.toLowerCase()
+      .includes(searchQuery.toLowerCase()));
+    setFilteredCountries(filtered);
+  }, [countries, searchQuery]);
+
   return (
     <div>
-      <h2>Country List</h2>
+      <h2>Country Population</h2>
+      <div className="country-search">
+        <input
+          type="text"
+          placeholder="Search country..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
       {loading === 'pending' && <p>Loading...</p>}
       <div className="country-grid">
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <div key={country.cca3} className="country-box">
             <Link to={`/country/${country.cca3}`} className="country-button">
               <img src={country.flags[1]} alt={country.name.common} />
