2a196a20595d6f7e0a54318523c166642d6b7fdf
Haider <malikhaiderkhan81@gmail.com>
2023-08-24 01:37:45 +0500

    display the list of countries and their data

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCountryData } from '../redux/countries/countriesSlice';
+import './CountryList.css';
 
 function CountryList() {
   const dispatch = useDispatch();
@@ -16,15 +17,20 @@ function CountryList() {
     <div>
       <h2>Country List</h2>
       {loading === 'pending' && <p>Loading...</p>}
-      <ul>
+      <div className="country-grid">
         {countries.map((country) => (
-          <li key={country.cca3}>
+          <div key={country.cca3} className="country-box">
             <Link to={`/country/${country.cca3}`} className="country-button">
-              {country.name.common}
+              <img src={country.flags[1]} alt={country.name.common} />
+              <h3>{country.name.common}</h3>
+              <p>
+                Population:
+                {country.population}
+              </p>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
