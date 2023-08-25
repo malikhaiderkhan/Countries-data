f2dcc2c933e82c3a6e69a311f301f37923f0f5e8
Haider <malikhaiderkhan81@gmail.com>
2023-08-25 21:18:14 +0500

    tests updated

--- src/__test__/CountryList.test.js
+++ src/__test__/CountryList.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
 import configureMockStore from 'redux-mock-store';
 import CountryList from '../components/CountryList';
 
--- src/__test__/header.test.js
+++ src/__test__/header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
 import configureMockStore from 'redux-mock-store';
 import Header from '../components/header';
 
